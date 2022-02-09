a4d16479d82c062decee943169941cda39eab32e
zaid9696 <zw283@hotmail.com>
2022-02-09 13:58:10 +0300

    domain test cookie 6

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,6 @@ const createToken = (user, statusCode, req, res) => {
 		expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
 		httpOnly: true,
 		sameSite: 'none',
-		domain: 'netlify.app',
 		secure: true
 	}
 
